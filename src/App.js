4517522b2b4ae423211a0cdb606e0446ab4395d2
miphc42 <philhc02@gmail.com>
2021-05-04 16:42:34 -0400

    commit main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import json_data from './MOCK_DATA.json';
 import './App.css';
 
+
 function App() {
+  const [input, setInput] = useState('');
+  useEffect(() => {
+
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Search..." 
+        onChange={event => setInput(event.target.value)}
+      />
+      <button onClick={() => {
+        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=820dd71f&s=${input}&type=movie`)
+        .then(response =>
+          response.json())
+        .then((result) => {
+          console.log(result)
+        })
+      }}>Click me</button>
     </div>
-  );
+  )
 }
 
 export default App;
