c3dc5ec8e6ae5b46235bf0385fd0940290307e93
miphc42 <philhc02@gmail.com>
2021-05-04 17:20:14 -0400

    fetch working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, {useEffect, useState} from 'react';
-import json_data from './MOCK_DATA.json';
 import './App.css';
 
 
 function App() {
   const [input, setInput] = useState('');
+  const [movie, setMovie] = useState('');
+  const [data, setData] = useState([]);
   useEffect(() => {
+      fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=820dd71f&s=${input}&type=movie`)
+      .then(response =>
+        response.json())
+      .then((result) => {
+        setData(result)
+      })
+  }, [movie])
 
-
-  }, [])
+  console.log(data.Search);
   return (
     <div className="App">
       <input type="text" placeholder="Search..." 
         onChange={event => setInput(event.target.value)}
       />
-      <button onClick={() => {
-        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=820dd71f&s=${input}&type=movie`)
-        .then(response =>
-          response.json())
-        .then((result) => {
-          console.log(result)
+      <button onClick={() => setMovie(input)}>Search</button>
+      {data.Search &&
+        data.Search.map((val, key) => {
+          return (
+            <div className="movie" key={key}>
+              <p>{val.Title}</p>
+            </div>
+          );
         })
-      }}>Click me</button>
+      }
     </div>
   )
 }
